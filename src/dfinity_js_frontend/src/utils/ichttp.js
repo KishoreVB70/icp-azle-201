c155d3c144513d2aef31f8c28f4ad8d27e6ee1b7
KishoreVB70 <kishore07kzr@gmail.com>
2024-05-31 08:25:48 +0530

    added comments for ichttp

---
+++ src/dfinity_js_frontend/src/utils/ichttp.js
@@ -0,0 +1,93 @@
+import { logout } from "./auth";
+
+class IcHttp {
+    // # is an indication of private fields, these fields can only be accessed within the class
+    #agent;
+    #decoder;
+    #encoder;
+
+    // Only requires the agent as input
+    // Agent could be a HTTP client or an API 
+    constructor(agent) {
+        this.#agent = agent;
+        this.#decoder = new TextDecoder('utf-8');
+        this.#encoder = new TextEncoder();
+    }
+
+    async GET(req) {
+        return await this.#doRequest(req.path, "GET", req.params);
+    }
+
+    async POST(req) {
+        return await this.#doRequest(req.path, "POST", req.params, req.data);
+    }
+
+    // Internal function of the class
+    async #doRequest(path, method, params, data) {
+        try {
+            const queryParams = new URLSearchParams(params ? params : {});
+            const url = params ? `${path}?${queryParams}` : path;
+            let response;
+            switch (method) {
+                // Get method doesn't have a body and headers
+                case "GET":
+                    // Await the respone from the backend
+                    response = await this.#agent.http_request({
+                        // The URL would contain the required information
+                        url,
+                        method,
+                        body: [],
+                        headers: [],
+                        certificate_version: [],
+                    });
+                    // Returns the result of the request( respone from the backend )
+                    return this.#parseResponse(response);
+                
+                // Other methods have body and header
+                case "POST":
+                // PUT and DELTE not declared, but still
+                case "PUT":
+                case "DELETE":
+                    // Convert the data into json format
+                    const body = data ? this.#encoder.encode(JSON.stringify(data)) : [];
+                    response = await this.#agent.http_request_update({
+                        url,
+                        method,
+                        body,
+                        // Basic header
+                        headers: [['Content-Type', 'application/json; charset=utf-8'], ['Content-Length', `${body.length}`]],
+                        certificate_version: [],
+                    });
+                    return this.#parseResponse(response);
+                default:
+                    throw new Error(`Unknown method: ${method}`);
+            }
+        } catch (err) {
+            if (err.name === 'AgentHTTPResponseError') {
+                logout();
+            }
+        }
+    }
+
+    #parseResponse(response) {
+        try {
+            // Checks for the status code, hence I need to set all the response to 200 in the backend
+            const body = this.#decoder.decode(response.body);
+
+            // If not succesful, throw the error
+            if (response.status_code !== 200) {
+                throw new Error(body);
+            }
+            const contentType = response.headers.filter(header => "content-type" === header[0].toLowerCase()).map(header => header[1]);
+            
+            // if everything is correct
+            if (contentType && contentType.length === 1 && contentType[0].toLowerCase() === 'application/json; charset=utf-8') {
+                return JSON.parse(body);
+            }
+            return body;
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+export default IcHttp;
